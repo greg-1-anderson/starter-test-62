bd67f55b6c12f903351e518be893547fa0faf248
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-04-08 07:28:15 -0700

    Modifications to template project from customization process.

--- src/Example.php
+++ src/Example.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ExampleProject;
+namespace StarterTest62;
 
 class Example
 {
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ExampleProject;
+namespace StarterTest62;
 
 use PHPUnit\Framework\TestCase;
 
